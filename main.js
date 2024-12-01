62c1960c80dd326d4fc3cd6275d6192bfa71e2ee
hgpark <digitalworks_hgpark@amorepacific.com>
2024-12-02 08:42:34 +0900

    기본 리소스 세팅

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const vh = window.innerHeight;
+  document.documentElement.style.setProperty("--vh", `${vh * 0.01}px`);
+});
