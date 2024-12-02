3f452f989550c7d12b9d308cbd2c24b96b261d68
hgpark <digitalworks_hgpark@amorepacific.com>
2024-12-02 14:58:03 +0900

    섹션1 pc완료

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 window.addEventListener("load", () => {
 
+  const vh =  window.innerHeight;
+  document.querySelector(".section01 .txtContainer").classList.add("on");
+  document.querySelector(".section01 .mainItem").classList.add("on");
+
+  window.addEventListener("scroll", (e) => {
+    console.log(window.scrollY/ (vh / 2))
+    const itemWrapper = document.querySelector(".section01 .itemWrapper");
+    const txtContainer = document.querySelector(".section01 .txtContainer");
+    const isScroll = (window.scrollY / (vh / 2)) > 0.7;
+    txtContainer.style.opacity = isScroll > 0.7 ? 0 : 1;
+    itemWrapper.style.opacity = isScroll > 0.7 ? 0 : 1;
+  });
 });
 
 document.addEventListener("DOMContentLoaded", () => {
